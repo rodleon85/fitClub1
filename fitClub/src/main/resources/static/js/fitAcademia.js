16e3517fb6d35bfe8caf1866e59155fd4bd77dcf
rodleon85 <54608093+rodleon85@users.noreply.github.com>
2019-10-12 00:05:39 -0300

    merge

--- fitClub/src/main/resources/static/js/fitAcademia.js
+++ fitClub/src/main/resources/static/js/fitAcademia.js
@@ -1,3 +1,56 @@
-$('.btnModal').click(function{
-	$('.modalPagamento').show();
-})
+$( document ).ready(function() {
+	
+	
+	if (acao == '/adicionarAluno'){
+		$('.cpf').mask('000.000.000-00');
+		
+		$('.telefone').mask('(00)0000-00000').focusout(function (event) {  
+            var target, phone, element;  
+            target = (event.currentTarget) ? event.currentTarget : event.srcElement;  
+            phone = target.value.replace(/\D/g, '');
+            element = $(target);  
+            element.unmask();  
+            if(phone.length > 10) {  
+                element.mask("(00) 00000-0000");  
+            } else {  
+                element.mask("(00) 0000-0000");  
+            }  
+        });
+		
+		$('.email').focusout(function(){
+			var valorEmail = $('.email').val();
+			if (!isEmail(valorEmail)){
+				$('.email').val('');
+			}
+		});
+		
+		
+	}
+	
+	if (acao == '/adicionarPagamento'){
+		$('.valor').attr("placeholder", " ");
+		$('.valor').attr("disabled", "true");
+		
+		
+		$('.tipoPagamento').click(function() {
+			if ($("input[name='tipoPagamento']:checked").val() == "mensal") {
+				$('.valor').attr("placeholder", "R$");
+				$('.valor').removeAttr("disabled");
+			}
+			if ($("input[name='tipoPagamento']:checked").val() == "ferias"){
+				$('.valor').attr("placeholder", "Dias");
+				$('.valor').removeAttr("disabled");
+			} 
+		})
+	}
+	
+});
+
+function isEmail(email) {
+	  var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	  if(!regex.test(email)) {
+	    return false;
+	  }else{
+	    return true;
+	  }
+	}
